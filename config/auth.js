c938a8e4e475ae7f37b457f2d2d213930b5ee2dc
CLMBN <juanmiguel.ossa+git@gmail.com>
2017-05-13 10:10:35 -0400

    https for highcharts

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'googleAuth' : {
 		'clientID': '377666810618-a4bntfqgg38p3uqg8kpo5drojqnv7b14.apps.googleusercontent.com',
 		'clientSecret': 'KufB02C3dwyJeBcUMA_0nm7k',
-		'callbackURL': 'http://localhost:8080/auth/google/callback'
+		'callbackURL': 'https://localhost:8080/auth/google/callback'
 	}
 }
